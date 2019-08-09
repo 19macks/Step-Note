702b55a95231157e78b25f14a8201bda90eed1a7
19macks <19macks@ukr.net>
2019-08-09 15:55:34 +0300

    added todo-list

--- routes/todo-list.js
+++ routes/todo-list.js
@@ -2,6 +2,21 @@ const app = require('express');
 const router = app.Router();
 const List = require('../models/list');
 
+// view and edit list
+router.get('/lists/:id', async (req, res) => {
+    let list = await List.findById(req.params.id);
+    console.log(list);
+    res.render('list-details', { list })
+});
+
+// save list to database
+router.post('/api/lists', async (req, res) => {
+    let list = new List (req.body);
+    await list.save((err) => {
+        if (err) return console.error(err);
+        res.redirect('/')
+    })
+});
 
 
 module.exports = router;
